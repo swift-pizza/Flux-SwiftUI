aa201b8e037a78a9d6de11cc3aed79a059b34060
Daniele Bogo <me@bogodaniele.com>
2019-12-12 16:42:42 +0000

    Add project and Pod

---
+++ Flux-SwiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Flux-SwiftUI
+//
+//  Created by Daniele Bogo on 12/12/2019.
+//  Copyright © 2019 Daniele Bogo. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Podfile
@@ -0,0 +1,16 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'Flux-SwiftUI' do
+  # Comment the next line if you don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for Flux-SwiftUI
+  pod 'WordPressFlux', '~> 1.0.1-beta'
+
+  target 'Flux-SwiftUIUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
